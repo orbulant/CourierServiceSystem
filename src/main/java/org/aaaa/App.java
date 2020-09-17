7ee75d23419be4c9052cf88d6b8ea9f002b21a7d
Rainwright <nicholaschen191100@gmail.com>
2020-09-17 17:38:29 +0800

    Added VSCode Debugging JSON

--- src/main/java/org/aaaa/App.java
+++ src/main/java/org/aaaa/App.java
@@ -1,13 +1,21 @@
 package org.aaaa;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+    // main removed from here as it caused problems using maven
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO Auto-generated method stub
+        stage = new Stage();
+        stage.setTitle("Sample");
+        stage.show();
     }
+    
 }
---
+++ src/main/java/org/aaaa/Main.java
@@ -0,0 +1,11 @@
+package org.aaaa;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main( String[] args )
+    {
+        // System.out.println( "Hello World!" );
+        Application.launch(App.class, args);
+    }
+}
