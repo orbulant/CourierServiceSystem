455158d91606f0495151e92d2c3195bf8b796ec8
Rainwright <nicholaschen191100@gmail.com>
2020-09-22 18:05:22 +0800

    Added Sample FXML and Controller

---
+++ src/main/java/org/aaaa/DashboardController.java
@@ -0,0 +1,22 @@
+package org.aaaa;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class DashboardController {
+    private Scene scene;
+    private Parent root;
+
+    public Scene getScene() {
+        try{
+            root = FXMLLoader.load(getClass().getResource("/fxml/Dashboard.fxml"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        scene = new Scene(root);
+
+        return this.scene;
+    }
+}
