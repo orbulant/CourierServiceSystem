53d05a601f4e6a6c5853baf3133471a2ef42fc5b
Rainwright <Rainwright@users.noreply.github.com>
2020-10-03 23:17:48 +0800

    Completed Order UI

--- src/main/java/org/aaaa/Controller/DashboardController.java
+++ src/main/java/org/aaaa/Controller/DashboardController.java
@@ -7,20 +7,46 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class DashboardController implements Initializable {
     @FXML
     AnchorPane mainPane;
+    @FXML
+    AnchorPane contentPane;
+    @FXML
+    GridPane dashboardPane;
+    @FXML
+    Button sidebarOrderButton;
+    @FXML
+    Button sidebarHomeButton;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        // TODO Auto-generated method stub
         try{
-            mainPane.getChildren().add((Node)FXMLLoader.load(getClass().getResource("/fxml/Sidebar.fxml")));
             mainPane.getChildren().add((Node)FXMLLoader.load(getClass().getResource("/fxml/Topbar.fxml")));
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        sidebarHomeButton.setOnMouseClicked(e -> {
+            try{
+                contentPane.getChildren().remove(0);
+                contentPane.getChildren().add(dashboardPane);
+            } catch (Exception eButton) {
+                eButton.printStackTrace();
+            }
+        });
+
+        sidebarOrderButton.setOnMouseClicked(e -> {
+            try{
+                contentPane.getChildren().remove(dashboardPane);
+                contentPane.getChildren().add((Node)FXMLLoader.load(getClass().getResource("/fxml/OrderForm.fxml")));
+            } catch (Exception eButton) {
+                eButton.printStackTrace();
+            }
+        });
     }
 }
---
+++ src/main/java/org/aaaa/Controller/OrderController.java
@@ -0,0 +1,18 @@
+package org.aaaa.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class OrderController implements Initializable {
+    @FXML
+    AnchorPane mainPane;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // System.out.println("Hi");
+    }
+}
