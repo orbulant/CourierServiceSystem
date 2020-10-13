fd1e436e8052d96521911d486ab0b0b8c8b9a075
Orbulant <personal@tankashing.com>
2020-10-13 14:32:40 +0800

    changed filehandler, added the complete fields, getters & setters to person class and staff class

--- src/main/java/org/aaaa/Person.java
+++ src/main/java/org/aaaa/Person.java
@@ -1,13 +1,23 @@
 package org.aaaa;
 
 import java.time.LocalDate;
-
+//*accountID,name,contact,nric,dob,houseNum,streetName,city,postcode,state,country,createdBy,createdOn,changedBy,changedOn*
 public class Person {
     protected String name;
     protected String contact;
     protected String nric;
     protected String email;
     protected LocalDate dob;
+    protected String num;
+    protected String houseNum;
+    protected String streetName;
+    protected String city;
+    protected String postcode;
+    protected String country;
+    protected String createdBy;
+    protected LocalDate createdOn;
+    protected String changedBy;
+    protected LocalDate changedOn;
 
     public Person() {
         this.name = "";
@@ -41,5 +51,109 @@ public class Person {
 
     public void setContact(String contact) {
         this.contact = contact;
+    }
+
+    public String getNric() {
+        return nric;
+    }
+
+    public void setNric(String nric) {
+        this.nric = nric;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getHouseNum() {
+        return houseNum;
+    }
+
+    public void setHouseNum(String houseNum) {
+        this.houseNum = houseNum;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDate getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDate createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getChangedBy() {
+        return changedBy;
+    }
+
+    public void setChangedBy(String changedBy) {
+        this.changedBy = changedBy;
+    }
+
+    public LocalDate getChangedOn() {
+        return changedOn;
+    }
+
+    public void setChangedOn(LocalDate changedOn) {
+        this.changedOn = changedOn;
     }
 }
--- src/main/java/org/aaaa/Staff.java
+++ src/main/java/org/aaaa/Staff.java
@@ -1,9 +1,17 @@
 package org.aaaa;
 
+import java.time.LocalDate;
+//*accountID,email,password,role,createdBy,createdOn,changedBy,changedOn*
 public class Staff extends Person {
     protected String username;
     protected String password;
     protected String role;
+    protected String accountID;
+    protected String createdBy;
+    protected LocalDate createdOn;
+    protected String changedBy;
+    protected LocalDate changedOn;
+
 
     public Staff() {
         this.username = "";
@@ -15,5 +23,69 @@ public class Staff extends Person {
         this.username = username;
         this.password = password;
         this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDate getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDate createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getChangedBy() {
+        return changedBy;
+    }
+
+    public void setChangedBy(String changedBy) {
+        this.changedBy = changedBy;
+    }
+
+    public LocalDate getChangedOn() {
+        return changedOn;
+    }
+
+    public void setChangedOn(LocalDate changedOn) {
+        this.changedOn = changedOn;
     }
 }
